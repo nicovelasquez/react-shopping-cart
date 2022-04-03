9456924ccd3c602bf1d1e81aef7e6a7cc0f6ee7f
nicovelasquez <n.velasquezp@alumnos.duoc.cl>
2022-04-03 10:48:20 -0400

    First

---
+++ src/components/Button.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+const styles = {
+    button: {
+        backgroundColor: '#0A283E',
+        color: '#fff',
+        padding: '15px 20px',
+        borderRadius: '5px',
+        border: 'none',
+        cursor: 'pointer'
+    }
+}
+
+class Button extends Component {
+    render() {
+        return <button style={styles.button} {...this.props} />
+    }
+}
+
+export default Button
